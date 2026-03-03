26b7255fd4a94ffed43cbbb628eed96855908fbf
Sergio Ferreira Ruiz <sergio.ferreira.ruiz.alu@iesfernandoaguilar.es>
2026-03-03 18:09:24 +0100

    Modificación del nombre de la aplicación

--- StaminApp/asgi.py
+++ StaminApp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for StaminaApp project.
+ASGI config for StaminApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StaminaApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StaminApp.settings')
 
 application = get_asgi_application()
--- StaminApp/wsgi.py
+++ StaminApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for StaminaApp project.
+WSGI config for StaminApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StaminaApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StaminApp.settings')
 
 application = get_wsgi_application()
